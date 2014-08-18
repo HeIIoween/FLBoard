aaa232b015b015d52e0844b5b3a681915459a8c7
raincious <raincious@gmail.com>
2014-08-19 02:54:58 +0800

    Add something forgotten.

--- Source/Sync.cpp
+++ Source/Sync.cpp
@@ -38,6 +38,8 @@ namespace raincious
 						InitializeCriticalSection(&staticQueueLock);
 					}
 
+					releasing = false;
+
 					EnterCriticalSection(&staticQueueLock);
 
 					Client* client = new Client(clientLogin, serverInfo);
